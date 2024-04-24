a24eae1fec2fa8675ee89615dc19d19eb2723cd4
veasna koeun <veasnakoeun@gmail.com>
2024-04-24 21:16:25 +0700

    adding code

---
+++ Reactjs/react-frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2> Let start build fullstack app with react and node js.</h2>
+    </div>
+  );
+}
+
+export default App;
